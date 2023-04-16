17c141d96c7f3f7d17c3423a00448fabf3c24d72
Juan <jrmediavilla@gmail.com>
2023-04-16 22:31:13 +0100

    Colors

--- components/ElementsGrid.js
+++ components/ElementsGrid.js
@@ -16,18 +16,41 @@ export default function ElementsGrid({ elements }) {
         // Limit the atomic mass to 5 digits in total and remove trailing zeros
         const atomicMass = Number(Number(element.atomic_mass).toFixed(digitsAfterDecimal));
 
+        function getCategoryClassName(category) {
+          const categoryClassMap = {
+            // Add other category mappings here
+            'diatomic nonmetal': styles.reactiveNonMetals,
+            'polyatomic nonmetal': styles.reactiveNonMetals,
+            'alkali metal': styles.AlkaliMetals,
+            'alkaline earth metal': styles.AlkalineEarthMetals,
+            'transition metal': styles.TransitionMetals,
+            'lanthanide': styles.lanthanide,
+            'lanthanide lanthanum': styles.lanthanum,
+            'actinide': styles.actinide,
+            'actinide actinium': styles.actinium,
+            'post-transition metal': styles.postTransitionMetal,
+            'metalloid': styles.metalloid,
+            'noble gas': styles.nobleGas,
+            'unknown probably transition metal': styles.unknownProperties,
+            'unknown probably post-transition metal': styles.unknownProperties,
+            'unknown probably metalloid': styles.unknownProperties,
+            'unknown predicted to be noble gas': styles.unknownProperties
+          };
+
+          return categoryClassMap[category] || '';
+        }
 
         return (
           <div
             key={element.number}
-            className={styles.element}
+            className={`${styles.element} ${element.category} ${getCategoryClassName(element.category)}`}
             style={{
               gridColumn: element.col18Xpos,
               gridRow: element.col18Ypos,
             }}
 
           >
-            <div className={styles.elementCardMedium}>
+            <div className={`${styles.elementCardMedium}`}>
               <div className={styles.atomicNumber}>{element.number}</div>
               <div className={styles.symbol}>{element.symbol}</div>
               <div className={styles.name}>{element.name}</div>
