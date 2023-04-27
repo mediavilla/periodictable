75c8876e1cc901337a2373b4b496503c6a210c62
Juan <jrmediavilla@gmail.com>
2023-04-27 23:31:57 +0100

    updated timeline, update on scroll still not working

--- pages/index.js
+++ pages/index.js
@@ -6,58 +6,58 @@ import ElementsGrid from '../components/ElementsGrid';
 import Timeline from '../components/Timeline';
 import TimelineContent from '../components/TimelineContent';
 
-// Sort the timeline data in descending order based on the year
 const sortedTimeline = timeline.sort((a, b) => b.year - a.year);
 
-function scrollToYear(year) {
-  const section = document.getElementById(`${year}`);
-  if (section) {
-    section.scrollIntoView({ behavior: "smooth" });
-  }
-}
-
-
-export default function App() {
-
+function App() {
   const [selectedYear, setSelectedYear] = useState(null);
 
   function scrollToYear(year) {
     const section = document.getElementById(`${year}`);
     if (section) {
       section.scrollIntoView({ behavior: 'smooth' });
-      setSelectedYear(year); // Update the selectedYear state
+      setSelectedYear(year);
     }
   }
 
   useEffect(() => {
-
     const handleScroll = () => {
-      // Your scroll handling logic here
-      const sections = document.querySelectorAll(".milestone-section");
+      console.log('Scroll event detected');
+      const sections = document.querySelectorAll('.milestone-section');
       const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-      const middleScreenPosition = scrollTop + (window.innerHeight / 2);
 
       for (const section of sections) {
-        if (middleScreenPosition >= section.offsetTop && middleScreenPosition < section.offsetTop + section.offsetHeight) {
-          // Update the selected year
+        console.log('Section ID:', section.id);
+        console.log('Section offsetTop:', section.offsetTop);
+        console.log('ScrollTop:', scrollTop);
+        console.log('Section offsetTop + section.offsetHeight:', section.offsetTop + section.offsetHeight);
+        if (
+          scrollTop + window.innerHeight / 2 >= section.offsetTop &&
+          scrollTop + window.innerHeight / 2 < section.offsetTop + section.offsetHeight
+        ) {
+
+          console.log('Setting selected year:', section.id);
           setSelectedYear(section.id);
+
           break;
         }
       }
     };
-    window.addEventListener("scroll", handleScroll);
+
+    window.addEventListener('scroll', handleScroll);
+
     return () => {
-      window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener('scroll', handleScroll);
     };
   }, []);
-
-
 
   return (
     <>
       <Head>
         <title>Create Next App</title>
-        <meta name="description" content="The evolution of the periodic table and disovery of each element" />
+        <meta
+          name="description"
+          content="The evolution of the periodic table and discovery of each element"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -71,8 +71,9 @@ export default function App() {
         <ElementsGrid elements={elements} />
 
         <TimelineContent yearsData={sortedTimeline} />
-
       </main>
     </>
-  )
+  );
 }
+
+export default App;
