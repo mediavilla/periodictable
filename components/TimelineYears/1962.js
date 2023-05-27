31a888c809452b54c2dcd30a9457da84d2edb16c
Juan <jrmediavilla@gmail.com>
2023-05-27 22:04:40 +0100

    1962 image

--- components/TimelineYears/1962.js
+++ components/TimelineYears/1962.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import Image from 'next/image';
 
 export default function Section1962({ description, elements, getCategoryClassName, selectedYear }) {
     return (
         <section id="1962" className="milestone-section">
             <h3><span className={`${selectedYear === 1962 ? 'selectedYear' : 'year'}`}>1962 /</span> {description}</h3>
             {/* Additional content */}
+
+
+
+            <Image
+                src="/images/PtF6.png"
+                width={439}
+                height={434}
+                alt="Platinum(VI) fluoride; Platinum hexafluoride"
+            />
         </section >
     );
 }
