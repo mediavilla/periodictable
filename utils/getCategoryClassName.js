ba653dccf665aea1afca768831ef8c3ae1d7dc5d
Juan <jrmediavilla@gmail.com>
2023-06-16 23:58:23 +0100

    Update getCategoryClassName.js

--- utils/getCategoryClassName.js
+++ utils/getCategoryClassName.js
@@ -4,20 +4,23 @@ function getCategoryClassName(category, isSelected) {
   const categoryClassMap = {
     // Add other category mappings here
     'diatomic nonmetal': isSelected ? `${elementStyles.reactiveNonMetals} ${elementStyles.selected}` : elementStyles.reactiveNonMetals,
-    'alkali metal': elementStyles.AlkaliMetals,
-    'alkaline earth metal': elementStyles.AlkalineEarthMetals,
-    'transition metal': elementStyles.TransitionMetals,
-    'lanthanide': elementStyles.lanthanide,
-    'lanthanide lanthanum': elementStyles.lanthanum,
-    'actinide': elementStyles.actinide,
-    'actinide actinium': elementStyles.actinium,
-    'post-transition metal': elementStyles.postTransitionMetal,
-    'metalloid': elementStyles.metalloid,
-    'noble gas': elementStyles.nobleGas,
-    'unknown probably transition metal': elementStyles.unknownProperties,
-    'unknown probably post-transition metal': elementStyles.unknownProperties,
-    'unknown probably metalloid': elementStyles.unknownProperties,
-    'unknown predicted to be noble gas': elementStyles.unknownProperties
+
+    'alkali metal': isSelected ? `${elementStyles.AlkaliMetals} ${elementStyles.selected}` : elementStyles.AlkaliMetals,
+    'alkaline earth metal': isSelected ? `${elementStyles.AlkalineEarthMetals} ${elementStyles.selected}` : elementStyles.AlkalineEarthMetals,
+    'transition metal': isSelected ? `${elementStyles.TransitionMetals} ${elementStyles.selected}` : elementStyles.TransitionMetals,
+    'lanthanide': isSelected ? `${elementStyles.lanthanide} ${elementStyles.selected}` : elementStyles.lanthanide,
+    'lanthanide lanthanum': isSelected ? `${elementStyles.lanthanum} ${elementStyles.selected}` : elementStyles.lanthanum,
+    'actinide': isSelected ? `${elementStyles.actinide} ${elementStyles.selected}` : elementStyles.actinide,
+    'actinide actinium': isSelected ? `${elementStyles.actinium} ${elementStyles.selected}` : elementStyles.actinium,
+    'post-transition metal': isSelected ? `${elementStyles.postTransitionMetal} ${elementStyles.selected}` : elementStyles.postTransitionMetal,
+    'metalloid': isSelected ? `${elementStyles.metalloid} ${elementStyles.selected}` : elementStyles.metalloid,
+    'noble gas': isSelected ? `${elementStyles.nobleGas} ${elementStyles.selected}` : elementStyles.nobleGas,
+    'unknown probably transition metal': isSelected ? `${elementStyles.unknownProperties} ${elementStyles.selected}` : elementStyles.unknownProperties,
+    'unknown probably post-transition metal': isSelected ? `${elementStyles.unknownProperties} ${elementStyles.selected}` : elementStyles.unknownProperties,
+    'unknown probably metalloid': isSelected ? `${elementStyles.unknownProperties} ${elementStyles.selected}` : elementStyles.unknownProperties,
+    'unknown predicted to be noble gas': isSelected ? `${elementStyles.unknownProperties} ${elementStyles.selected}` : elementStyles.unknownProperties,
+
+
   };
 
   return categoryClassMap[category] || '';
