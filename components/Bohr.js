68198b245e30b8af1e1968dbfe706873b39b419b
Juan <jrmediavilla@gmail.com>
2023-06-21 22:59:50 +0100

    Bohr model - first try 😎

--- components/Bohr.js
+++ components/Bohr.js
@@ -1,10 +1,100 @@
 import React from 'react';
 
-export default function Bohr() {
+export default function Bohr({ element }) {
+
+    const atomicMass = element.atomic_mass.toFixed(0)
+    const numberOfShells = element.shells.length
+    const numberOfNeutrons = atomicMass - element.number
+    const svgSize = 100 + (numberOfShells * 35)
+
+
+    function electrons(electron, orbit, diameter, radius, numberElectronsInShell) {
+        const angle = (electron / (numberElectronsInShell / 2)) * Math.PI // Calculate the angle at which the element will be placed.
+        const x = (radius * Math.cos(angle)) + (diameter / 2) // Calculate the x position of the element.
+        const y = (radius * Math.sin(angle)) + (diameter / 2) // Calculate the y position of the element.
+
+        return (
+            <circle
+                key={"" + electron + orbit}
+                cx={((svgSize / 2) - radius) + x}
+                cy={((svgSize / 2) - radius) + y}
+                r="6"
+                fill="#000000"
+            />
+        )
+    }
+
+    function orbits(radius, orbit) {
+        return (
+            <circle key={orbit}
+                cx={svgSize / 2}
+                cy={svgSize / 2}
+                r={radius}
+                stroke="white"
+                strokeWidth="1"
+                fill="none"
+            />
+        )
+    }
+
+    function shellsOrbits() {
+
+        let children = []
+
+        for (let i = 0; i <= numberOfShells - 1; i++) {
+            let numberElectronsInShell = element.shells[i]
+            let radius, diameter
+
+            if (i === 0) { // Just one orbit, like Hydrogen
+                radius = 40
+                diameter = radius * 2
+            } else {
+                radius = (i * 20) + 40
+                diameter = radius * 2
+            }
+
+            children.push(orbits(radius, i))
+
+            for (let j = 1; j <= numberElectronsInShell; j++) {
+                children.push(
+                    electrons(j, i, diameter, radius, numberElectronsInShell)
+                )
+            }
+        }
+
+        return children
+    }
+
     return (
-        <div>
-            <h1>Bohr</h1>
-            <p>Placeholder</p>
-        </div>
-    );
+
+        <div id="electronConfiguration" >
+            <svg width={svgSize} height={svgSize}>
+                <text x="50%"
+                    y="44%"
+                    textAnchor="middle"
+                    fill="#000000"
+                    fontSize="14px"
+                    fontFamily="Arial"
+                    dy=".3em">
+                    p
+                    <tspan baselineShift="super" fontSize="10px">+</tspan>
+                    {element.number}
+                </text>
+                <text x="50%"
+                    y="55%"
+                    textAnchor="middle"
+                    fill="#000000"
+                    fontSize="14px"
+                    fontFamily="Arial"
+                    dy=".3em">
+                    n
+                    <tspan baselineShift="super" fontSize="10px">o</tspan>
+                    {numberOfNeutrons}
+                </text>
+
+                {shellsOrbits()}
+            </svg>
+
+        </div >
+    )
 }
