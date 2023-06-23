56ba50d6f3dbe176a2d146162bdf6df5bd678045
Juan <jrmediavilla@gmail.com>
2023-06-23 23:10:31 +0100

    Hydrogen copy

--- components/Elements/Hydrogen.js
+++ components/Elements/Hydrogen.js
@@ -5,6 +5,19 @@ export default function Hydrogen() {
         <div>
             <h1>Hydrogen</h1>
             <p>Custom content for Hydrogen</p>
+            <h4>Discovery:</h4>
+            <p>Hydrogen was officially recognized as a distinct substance by Henry Cavendish in 1766, although earlier scientists such as Robert Boyle had produced and identified gases that were likely composed of hydrogen.</p>
+            <h4>Name Origin:**</h4>
+            <p>The name Hydrogen comes from the Greek words "hydro" meaning "water", and "genes" meaning "creator".This is because when Hydrogen burns in the air, it creates water.</p>
+            <h4>Natural Occurrence</h4>
+            <p>Hydrogen is the most abundant element in the universe! In its pure form on Earth, it is a gas that's rarely found because it is so light that Earth's gravity can't hold it down. However, you can find it combined with other elements in substances like water (H2O), where each water molecule includes two hydrogen atoms.</p>
+            <h4>Properties and Characteristics</h4>
+            <p>Hydrogen is the first element on the periodic [table.It](http://table.it/)'s the lightest element and is a gas at room temperature. It's colorless, odorless, and tasteless.Although it is not found freely in nature, it is part of many substances as we mentioned before.Hydrogen is also unique because it can exist in a number of different states, including as a metallic substance at extremely high pressures.</p>
+            <h4>Applications</h4>
+            <p>Hydrogen has many [uses.It](http://uses.it/)'s used in the production of ammonia for fertilizers, in the refining of fossil fuels, and in the production of methanol. It's also used in rocket fuel, and fuel cells, which can generate electricity and heat by combining hydrogen and oxygen.Additionally, hydrogen is used in the food industry to hydrogenate fats and oils, and in the glass industry as a protective atmosphere for making flat glass sheets.</p>
+            <h4>Interaction with other Elements</h4>
+            <p>Hydrogen has the special ability to combine with almost all other elements in the periodic table to form compounds, thanks to its single electron and ability to donate or accept an electron to achieve stability.One of its most significant bonds is with Oxygen, to form water(H2O).It also bonds with Carbon to form numerous organic compounds, including hydrocarbons like methane(CH4).Its combination with Nitrogen produces ammonia(NH3), which is a vital element for life as well.</p>
+            <p>Each of these different combinations has important uses in industry and life on Earth.For example, the combination of Hydrogen and Oxygen in water is essential for all known forms of life!</p>
         </div>
     );
 }
