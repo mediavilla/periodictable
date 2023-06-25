d37267b20b1ae2bcf536bf65d3c5cdbd169d769c
Juan <jrmediavilla@gmail.com>
2023-06-25 18:58:54 +0100

    Improve table switcher
    Maybe I should rename the table loader to table switcher

--- components/TableLoader.js
+++ components/TableLoader.js
@@ -3,19 +3,25 @@ import Table18Cols from './Table18Cols';
 import Table32Cols from './Table32Cols';
 
 export default function TableLoader({ elements }) {
-    const [currentTable, setCurrentTable] = useState("table1");
+    const [currentTable, setCurrentTable] = useState("table18");
 
-
+    function renderTable() {
+        switch (currentTable) {
+            case "table18":
+                return <Table18Cols elements={elements} />;
+            case "table32":
+                return <Table32Cols elements={elements} />;
+            // Add more case statements as you add more tables
+            default:
+                return <Table18Cols elements={elements} />; // Default to Table18Cols if currentTable doesn't match any case
+        }
+    }
 
     return (
         <div>
             <button onClick={() => setCurrentTable("table18")}>Table 18 columns</button>
             <button onClick={() => setCurrentTable("table32")}>Table 32 columns</button>
-            {currentTable === "table18" ? (
-                <Table18Cols elements={elements} />
-            ) : (
-                <Table32Cols elements={elements} />
-            )}
+            {renderTable()}
         </div>
     );
 }
