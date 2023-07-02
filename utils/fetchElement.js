b24ac289298df0833083dd8e9f0accaf3d884ea1
Juan <jrmediavilla@gmail.com>
2023-07-02 23:32:32 +0100

    elementsData was the same as elements to using elements from now on because it was confusing af

--- utils/fetchElement.js
+++ utils/fetchElement.js
@@ -1,8 +1,8 @@
-import elementsData from '../public/elements.json';
+import elements from '../public/elements.json';
 
 
 export default function fetchElement(elementName) {
-    const elementData = elementsData.find(el => el.name.toLowerCase() === elementName.toLowerCase());
+    const elementData = elements.find(el => el.name.toLowerCase() === elementName.toLowerCase());
 
     if (!elementData) {
         console.warn(`No element found for name: ${elementName}`);
