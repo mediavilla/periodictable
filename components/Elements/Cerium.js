b75ca8029055bbf3ff472a93e63da60e024a7342
Juan <jrmediavilla@gmail.com>
2023-07-09 21:42:29 +0100

    2 elements only

--- components/Elements/Cerium.js
+++ components/Elements/Cerium.js
@@ -4,7 +4,24 @@ export default function Cerium() {
     return (
         <div>
             <h1>Cerium</h1>
-            <p>Custom content for Cerium</p>
+            <h4>Discovery</h4>
+            <p>Cerium was discovered in 1803 by both Klaproth and Berzelius, two renowned chemists. They found it independently within the same year while working on an ore of cerium now known as cerite.</p>
+
+            <h4>Name Origin</h4>
+            <p>The name 'Cerium' is derived from the name of the dwarf planet Ceres, which was discovered just two years prior to cerium.</p>
+
+            <h4>Natural Occurrence</h4>
+            <p>Cerium is the most abundant of the rare-earth metals and is found in a number of minerals including allanite (also known as orthite), monazite, bastnasite, cerite, and samarskite.</p>
+
+            <h4>Properties and Characteristics</h4>
+            <p>Cerium is a soft, ductile, and silvery-white metal that tarnishes when exposed to air, and it is soft enough to be cut with a knife. It is the second element of the lanthanide series.</p>
+
+            <h4>Applications</h4>
+            <p>Cerium is used in various applications such as in carbon-arc lighting, especially in the motion picture industry. It's also a crucial component of misch metal, used in the manufacturing of lighter flints. Cerium oxide is used as a catalytic converter for the minimization of CO emissions in the automotive industry.</p>
+
+            <h4>Interaction with Other Elements</h4>
+            <p>Cerium readily reacts with oxygen and water. When mixed with iron, it creates flints for a lighter due to its pyrophoric nature, which means it can ignite spontaneously at room temperature.</p>
+
         </div>
     );
 }
--- components/Elements/Praseodymium.js
+++ components/Elements/Praseodymium.js
@@ -4,7 +4,24 @@ export default function Praseodymium() {
     return (
         <div>
             <h1>Praseodymium</h1>
-            <p>Custom content for Praseodymium</p>
+            <h4>Discovery</h4>
+            <p>Praseodymium was discovered in 1885 by Austrian scientist Carl Auer von Welsbach. He separated praseodymium, along with the element neodymium, from a material called didymium by using repeated fractional crystallization.</p>
+
+            <h4>Name Origin</h4>
+            <p>The name 'Praseodymium' comes from the Greek words 'prasios' and 'didymos' which together mean 'green twin'. This is due to the green salts the element produces and the fact it was discovered alongside its 'twin', neodymium.</p>
+
+            <h4>Natural Occurrence</h4>
+            <p>Praseodymium can be found in nature in minerals such as monazite and bastnasite, usually along with other rare earth metals.</p>
+
+            <h4>Properties and Characteristics</h4>
+            <p>Praseodymium is a soft, silvery, malleable, and ductile metal in the lanthanide group. It's valued for its magnetic, electrical, chemical, and optical properties. It will develop a green-tinged oxide layer when exposed to air.</p>
+
+            <h4>Applications</h4>
+            <p>Praseodymium is used to create high-strength metals for use in aircraft engines. It's also used in the glass industry to create yellow-colored glasses and ceramics, and to make certain types of goggles. Praseodymium alloys can be found in permanent magnets and flint lighters as well.</p>
+
+            <h4>Interaction with Other Elements</h4>
+            <p>Praseodymium reacts slowly with oxygen to form Praseodymium(III) oxide. It also reacts with the halogens to form trihalides, and will burn at 150 °C to form the nitride PrN.</p>
+
         </div>
     );
 }
