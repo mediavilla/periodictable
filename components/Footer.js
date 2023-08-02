254fd07b7053e97468f9ce9a4e8f7bdeac6d8c47
Juan <jrmediavilla@gmail.com>
2023-08-02 23:33:57 +0100

    Footer 2 columns

--- components/Footer.js
+++ components/Footer.js
@@ -6,22 +6,28 @@ export default function Footer() {
 
     return (
         <footer className={footerStyles.footer}>
-            <div id="content">
+            <div className={footerStyles.footerLinks}>
                 <ul className={footerStyles.footerList}>
-
                     <li><Link href="#">About</Link></li>
                     <li><Link href="#">Nerds</Link></li>
                     <li><Link href="#">Download app</Link></li>
                     <li><Link href="#">Shop</Link></li>
                     <li><Link href="#">Lab</Link></li>
                     <li><Link href="#">External resources</Link></li>
+                </ul>
+            </div>
+            <div className={footerStyles.footerLinks}>
+                <ul className={footerStyles.footerList}>
                     <li><Link href="#">Cookie Policy</Link></li>
                     <li><Link href="#">Terms and conditions</Link></li>
                     <li><Link href="#">License</Link></li>
-                    <li><Link href="#">Social icons</Link></li>
-
                 </ul>
             </div>
-        </footer>
+            <div className={footerStyles.footerLinks}>
+                <ul className={footerStyles.footerList}>
+                    <li><Link href="#">Social icons</Link></li>
+                </ul>
+            </div>
+        </footer >
     );
 }
