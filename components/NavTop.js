017320214f91f4f05f882d60419bc4509ab00147
Juan <jrmediavilla@gmail.com>
2023-08-04 23:45:24 +0100

    Top nav as inline links :)

--- components/NavTop.js
+++ components/NavTop.js
@@ -5,8 +5,8 @@ import navTopStyles from '../styles/navTop.module.css';
 export default function NavTop() {
 
     return (
-        <nav className="">
-            <ul className="">
+        <nav className={navTopStyles.navTop}>
+            <ul>
                 <li><Link href="#">Topologies</Link></li>
                 <li><Link href="#">Timeline</Link></li>
                 <li><Link href="#">Elements</Link></li>
