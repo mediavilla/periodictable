1d2deb53062c85d23b46dee4a1ae325d7f666695
Juan <jrmediavilla@gmail.com>
2023-08-08 23:43:14 +0100

    simplified nav
    add particles and scientist to the post MVP roadmap

--- components/Footer.js
+++ components/Footer.js
@@ -8,11 +8,9 @@ export default function Footer() {
         <footer className={footerStyles.footer}>
             <div className={footerStyles.footerLinks}>
                 <ul className={footerStyles.footerList}>
-                    <li><Link href="#">Topologies</Link></li>
+                    <li><Link href="#">Designs</Link></li>
                     <li><Link href="#">Timeline</Link></li>
                     <li><Link href="#">Elements</Link></li>
-                    <li><Link href="#">Particles</Link></li>
-                    <li><Link href="#">Scientists</Link></li>
                 </ul>
             </div>
             <div className={footerStyles.footerLinks}>
--- components/NavTop.js
+++ components/NavTop.js
@@ -7,11 +7,9 @@ export default function NavTop() {
     return (
         <nav className={navTopStyles.navTop}>
             <ul>
-                <li><Link href="#">Topologies</Link></li>
+                <li><Link href="#">Designs</Link></li>
                 <li><Link href="#">Timeline</Link></li>
                 <li><Link href="#">Elements</Link></li>
-                <li><Link href="#">Particles</Link></li>
-                <li><Link href="#">Scientists</Link></li>
             </ul>
         </nav >
     );
