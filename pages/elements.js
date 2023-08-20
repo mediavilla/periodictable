420c82780d8f2a7db81c8790a8baa6e9673fce39
Juan <jrmediavilla@gmail.com>
2023-08-20 22:34:04 +0100

    Notes for future page
    Not MVP

--- pages/elements.js
+++ pages/elements.js
@@ -10,6 +10,22 @@ export default function elements() {
                 <div id="content">
 
                     <NavTop />
+
+                    <p>There are 118 elements in the periodic table, these are the elements that make up everything in the universe, our planet, us</p>
+                    <p>Would be cool to get a few ways to filter elements in different ways:</p>
+                    <ul>
+                        <li>Elements present in the human body</li>
+                        <li>Elements present in nature / Earth</li>
+                        <li>Elements present in the universe</li>
+                        <ul>
+                            <li>The Moon</li>
+                            <li>Mars</li>
+                            <li>Other planets in the solar system</li>
+                        </ul>
+                        <li>What elements are the most easy to combine with others</li>
+                        <li>What elements are lighter / heavier</li>
+                        <li>Groupings of the elements</li>
+                    </ul>
                 </div>
             </main>
             <Footer />
