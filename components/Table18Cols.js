042c4a05f4f0038dd0590b015ece010186c56341
Juan <jrmediavilla@gmail.com>
2023-08-29 23:16:29 +0100

    Bg gradient with alpha and better order but can be better

--- components/Table18Cols.js
+++ components/Table18Cols.js
@@ -32,15 +32,23 @@ export default function Table18({ elements }) {
     return { topLeftElement, bottomLeftElement, topRightElement, bottomRightElement };
   }
 
+  function hexToRgba(hex, alpha) {
+    const r = parseInt(hex.slice(1, 3), 16);
+    const g = parseInt(hex.slice(3, 5), 16);
+    const b = parseInt(hex.slice(5, 7), 16);
 
+    return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+  }
+
+  const opacity = 0.2; // 20% opacity
 
   function updateBackgroundGradient(adjacentElements) {
-    const topLeftColor = getCategoryHexColor(adjacentElements.topLeftElement.category);
-    const bottomLeftColor = adjacentElements.bottomLeftElement ? getCategoryHexColor(adjacentElements.bottomLeftElement.category) : '#efefef';
-    const topRightColor = adjacentElements.topRightElement ? getCategoryHexColor(adjacentElements.topRightElement.category) : '#efefef';
-    const bottomRightColor = adjacentElements.bottomRightElement ? getCategoryHexColor(adjacentElements.bottomRightElement.category) : '#efefef';
+    const topLeftColor = hexToRgba(getCategoryHexColor(adjacentElements.topLeftElement.category), opacity);
+    const bottomLeftColor = adjacentElements.bottomLeftElement ? hexToRgba(getCategoryHexColor(adjacentElements.bottomLeftElement.category), opacity) : '#efefef';
+    const topRightColor = adjacentElements.topRightElement ? hexToRgba(getCategoryHexColor(adjacentElements.topRightElement.category), opacity) : '#efefef';
+    const bottomRightColor = adjacentElements.bottomRightElement ? hexToRgba(getCategoryHexColor(adjacentElements.bottomRightElement.category), opacity) : '#efefef';
 
-    const gradient = `conic-gradient(from 1.5708rad at 50% 50%, ${topLeftColor} 9%, ${topRightColor} 41%, ${bottomRightColor} 51%, ${bottomLeftColor} 92%)`;
+    const gradient = `conic-gradient(from 1.5708rad at 50% 50%, ${bottomRightColor} 9%, ${bottomLeftColor} 41%, ${topLeftColor} 51%, ${topRightColor} 92%)`;
     document.documentElement.style.backgroundImage = gradient;
   }
 
