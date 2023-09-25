97db403e282a7d1c496224f2df5cf7a501f54495
Juan <jrmediavilla@gmail.com>
2023-09-25 21:50:23 +0100

    Failed rectangles animation

--- components/CanvasBackground.js
+++ components/CanvasBackground.js
@@ -5,9 +5,22 @@ import { TableContext } from '../utils/TableProvider';
 function CanvasBackground({ colors = {} }) {
     const canvasRef = useRef(null);
 
-    const [rectangles, setRectangles] = useState([]); // To store the six rectangles
+    const [rectangles, setRectangles] = useState([]); // To store the six rectangles NOT SURE IF I NEED THIS
 
     const { currentElement, prevCol18Xpos, prevCol18Ypos } = useContext(TableContext);
+
+    // Step 1: Move rectangle position variables outside of useEffect
+    // Step 2: Use useState to manage positions
+    const [topLeft, setTopLeft] = useState({ x: 0, y: 0 });
+    const [topRight, setTopRight] = useState({ x: 0, y: 0 }); // Initialize properly later
+    const [bottomLeft, setBottomLeft] = useState({ x: 0, y: 0 }); // Initialize properly later
+    const [bottomRight, setBottomRight] = useState({ x: 0, y: 0 }); // Initialize properly later
+
+    // Define off-canvas rectangles
+    const [offCanvasTopLeft, setOffCanvasTopLeft] = useState({ x: 0, y: 0 }); // Initialize properly later
+    const [offCanvasTopRight, setOffCanvasTopRight] = useState({ x: 0, y: 0 }); // Initialize properly later
+    const [offCanvasBottomLeft, setOffCanvasBottomLeft] = useState({ x: 0, y: 0 }); // Initialize properly later
+    const [offCanvasBottomRight, setOffCanvasBottomRight] = useState({ x: 0, y: 0 }); // Initialize properly later
 
     // Function to determine the direction of mouse movement
 
@@ -48,6 +61,27 @@ function CanvasBackground({ colors = {} }) {
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
 
+
+        // Initialize positions if they are at (0, 0)
+        if (topLeft.x === 0 && topLeft.y === 0) {
+            setTopLeft({ x: 0, y: 0 });
+            setTopRight({ x: canvas.width / 2, y: 0 });
+            setBottomLeft({ x: 0, y: canvas.height / 2 });
+            setBottomRight({ x: canvas.width / 2, y: canvas.height / 2 });
+
+            setOffCanvasTopLeft({ x: 0, y: -canvas.height });
+            setOffCanvasTopRight({ x: canvas.width / 2, y: -canvas.height });
+            setOffCanvasBottomLeft({ x: 0, y: canvas.height * 2 });
+            setOffCanvasBottomRight({ x: canvas.width / 2, y: canvas.height * 2 });
+        }
+        // Determine the direction of movement
+        const direction = getDirection();
+
+        // Update positions based on direction
+        if (direction) {
+            updateRectanglePositions(direction, topLeft, topRight, bottomLeft, bottomRight, offCanvasTopLeft, offCanvasTopRight, offCanvasBottomLeft, offCanvasBottomRight, canvas);
+        }
+
         // Clear the canvas
         ctx.clearRect(0, 0, canvas.width, canvas.height);
 
@@ -55,28 +89,26 @@ function CanvasBackground({ colors = {} }) {
         ctx.filter = 'blur(150px)';
 
         // Draw the rectangles based on passed colors
+
         if (colors.topLeftColor) {
             ctx.fillStyle = colors.topLeftColor;
-            ctx.fillRect(0, 0, canvas.width / 2, canvas.height / 2);
+            ctx.fillRect(topLeft.x, topLeft.y, canvas.width / 2, canvas.height / 2);
         }
 
         if (colors.bottomLeftColor) {
             ctx.fillStyle = colors.bottomLeftColor;
-            ctx.fillRect(0, canvas.height / 2, canvas.width / 2, canvas.height / 2);
+            ctx.fillRect(bottomLeft.x, bottomLeft.y, canvas.width / 2, canvas.height / 2);
         }
 
         if (colors.topRightColor) {
             ctx.fillStyle = colors.topRightColor;
-            ctx.fillRect(canvas.width / 2, 0, canvas.width / 2, canvas.height / 2);
+            ctx.fillRect(topRight.x, topRight.y, canvas.width / 2, canvas.height / 2);
         }
 
         if (colors.bottomRightColor) {
             ctx.fillStyle = colors.bottomRightColor;
-            ctx.fillRect(canvas.width / 2, canvas.height / 2, canvas.width / 2, canvas.height / 2);
+            ctx.fillRect(bottomRight.x, bottomRight.y, canvas.width / 2, canvas.height / 2);
         }
-
-        // Determine the direction of movement
-        const direction = getDirection();
 
         // Trigger the animation
         if (direction) {
@@ -95,7 +127,9 @@ function CanvasBackground({ colors = {} }) {
         return () => {
             window.removeEventListener('resize', handleResize);
         };
-    }, [colors, currentElement, prevCol18Xpos, prevCol18Ypos]);
+    }, [colors, topLeft, topRight, bottomLeft, bottomRight, offCanvasTopLeft, offCanvasTopRight, offCanvasBottomLeft, offCanvasBottomRight, currentElement, prevCol18Xpos, prevCol18Ypos]);
+
+
 
     useEffect(() => {
         // Log the values in the browser's console
@@ -104,6 +138,67 @@ function CanvasBackground({ colors = {} }) {
         console.log("prevCol18Xpos: ", prevCol18Xpos);
         console.log("prevCol18Ypos: ", prevCol18Ypos);
     }, [currentElement, prevCol18Xpos, prevCol18Ypos]);
+
+
+    // CanvasBackground.js
+
+    const updateRectanglePositions = (direction, topLeft, topRight, bottomLeft, bottomRight, offCanvasTopLeft, offCanvasTopRight, offCanvasBottomLeft, offCanvasBottomRight, canvas) => {
+
+        const halfCanvasWidth = canvas.width / 2;
+        const halfCanvasHeight = canvas.height / 2;
+
+        switch (direction) {
+            case 'up':
+                // Move all rectangles down by canvas height
+                topLeft.y += halfCanvasHeight;
+                topRight.y += halfCanvasHeight;
+                bottomLeft.y += halfCanvasHeight;
+                bottomRight.y += halfCanvasHeight;
+
+                // Position the off-canvas rectangles at the top
+                offCanvasTopLeft.y = topLeft.y - canvas.height;
+                offCanvasTopRight.y = topRight.y - canvas.height;
+                break;
+
+            case 'down':
+                // Move all rectangles up by canvas height
+                topLeft.y -= halfCanvasHeight;
+                topRight.y -= halfCanvasHeight;
+                bottomLeft.y -= halfCanvasHeight;
+                bottomRight.y -= halfCanvasHeight;
+
+                // Position the off-canvas rectangles at the bottom
+                offCanvasBottomLeft.y = bottomLeft.y + canvas.height;
+                offCanvasBottomRight.y = bottomRight.y + canvas.height;
+                break;
+
+            case 'left':
+                // Move all rectangles to the right by canvas width
+                topLeft.x += halfCanvasWidth;
+                topRight.x += halfCanvasWidth;
+                bottomLeft.x += halfCanvasWidth;
+                bottomRight.x += halfCanvasWidth;
+
+                // Position the off-canvas rectangles on the left
+                offCanvasTopLeft.x = topLeft.x - canvas.width;
+                offCanvasBottomLeft.x = bottomLeft.x - canvas.width;
+                break;
+
+            case 'right':
+                // Move all rectangles to the left by canvas width
+                topLeft.x -= halfCanvasWidth;
+                topRight.x -= halfCanvasWidth;
+                bottomLeft.x -= halfCanvasWidth;
+                bottomRight.x -= halfCanvasWidth;
+
+                // Position the off-canvas rectangles on the right
+                offCanvasTopRight.x = topRight.x + canvas.width;
+                offCanvasBottomRight.x = bottomRight.x + canvas.width;
+                break;
+        }
+    };
+
+
 
     return (
         <canvas ref={canvasRef} style={{ position: 'fixed', top: 0, left: 0, zIndex: -2 }} />
