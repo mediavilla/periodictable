9a5cbadf910118a19d51ada38c7f5b293c9ea063
Juan <jrmediavilla@gmail.com>
2023-11-02 22:14:51 +0000

    Down the rabbit hole again :(

--- components/CanvasBackground.js
+++ components/CanvasBackground.js
@@ -9,6 +9,7 @@ const CanvasBackground = () => {
     const canvasRef = useRef(null);
     const [squares, setSquares] = useState([]); // This will hold both visible and off-canvas squares
     const { elements, currentElement, prevCol18Xpos, prevCol18Ypos } = useContext(TableContext);
+    let topLeftColor, bottomLeftColor, topRightColor, bottomRightColor;
 
 
     // #####################################################
@@ -78,13 +79,16 @@ const CanvasBackground = () => {
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
 
-        // Placeholder for initializing squares
+        // Placeholder for initializing squares with default colors or transparent
         const initialSquares = [
-            // Define your initial squares here with x, y, and color
+            { x: 0, y: 0, color: 'transparent' },
+            { x: canvas.width / 2, y: 0, color: 'transparent' },
+            { x: 0, y: canvas.height / 2, color: 'transparent' },
+            { x: canvas.width / 2, y: canvas.height / 2, color: 'transparent' }
         ];
         setSquares(initialSquares);
 
-        // Placeholder function to draw squares
+        // Function to draw squares
         const drawSquares = () => {
             ctx.clearRect(0, 0, canvas.width, canvas.height);
             initialSquares.forEach(square => {
@@ -94,7 +98,33 @@ const CanvasBackground = () => {
         };
 
         drawSquares();
-    }, []);
+    }, []); // Empty dependency array means this runs once on mount
+
+
+    // #####################################################
+    // #####################################################
+    // #####################################################
+    // Update colors when elements or currentElement change
+    useEffect(() => {
+        if (elements && currentElement) {
+            const { topLeftColor, bottomLeftColor, topRightColor, bottomRightColor } = getQuadrantColors(currentElement, elements);
+            // Update the colors of the squares
+            setSquares(squares => squares.map((square, index) => {
+                switch (index) {
+                    case 0: return { ...square, color: topLeftColor };
+                    case 1: return { ...square, color: topRightColor };
+                    case 2: return { ...square, color: bottomLeftColor };
+                    case 3: return { ...square, color: bottomRightColor };
+                    default: return square;
+                }
+            }));
+        }
+    }, [elements, currentElement]); // Dependencies on elements and currentElement
+
+
+
+
+
 
     // Method to handle hover events
     // #####################################################
@@ -126,6 +156,9 @@ const CanvasBackground = () => {
         animateSquares(direction, offCanvasSquares);
     };
 
+    // #####################################################
+    // #####################################################
+    // #####################################################
     // You would call handleHover in a useEffect that depends on currentElement
     useEffect(() => {
         handleHover();
@@ -134,10 +167,58 @@ const CanvasBackground = () => {
 
     // Animation logic
     const animateSquares = (direction, offCanvasSquares) => {
-        // Use anime.js to animate squares
-        // In the complete function, update the state with new positions and colors
-    };
+        // Define the animation targets
+        const targets = squares.concat(offCanvasSquares);
 
+        // Define the animation properties based on direction
+        let animationProperties;
+        switch (direction) {
+            case 'up':
+                animationProperties = { translateY: '-=canvas.height / 2' };
+                break;
+            case 'down':
+                animationProperties = { translateY: '+=canvas.height / 2' };
+                break;
+            case 'left':
+                animationProperties = { translateX: '-=canvas.width / 2' };
+                break;
+            case 'right':
+                animationProperties = { translateX: '+=canvas.width / 2' };
+                break;
+            default:
+                // If direction is not defined, we don't want to run the animation
+                return;
+        }
+
+
+        // #####################################################
+        // #####################################################
+        // #####################################################
+        // Run the animation with anime.js
+        anime({
+            targets: targets,
+            ...animationProperties,
+            easing: 'easeOutQuad',
+            duration: 1000, // Duration in milliseconds
+            complete: function (anim) {
+                // Once the animation is complete, update the state with new positions and colors
+                // You'll need to calculate the new positions based on the animation
+                // For example, if the direction is 'up', you'll subtract canvas.height / 2 from the y position of each square
+                const newSquares = targets.map(square => {
+                    // Calculate new position based on direction
+                    // This is a placeholder, you'll need to implement the logic based on your application's needs
+                    return {
+                        ...square,
+                        y: square.y - (direction === 'up' ? canvas.height / 2 : 0), // Example for 'up' direction
+                        // Add similar calculations for x and y based on other directions
+                    };
+                });
+
+                // Update the squares state with the new squares
+                setSquares(newSquares);
+            }
+        });
+    };
     // Render the canvas
     return (
         <canvas ref={canvasRef} style={{ position: 'fixed', top: 0, left: 0, zIndex: -2 }} />
