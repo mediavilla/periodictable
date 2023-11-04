214960e6a0fd0f1dd5526a06cfda6775a24a162d
Juan <jrmediavilla@gmail.com>
2023-11-04 08:19:15 +0000

    Comments added for this bitch

--- components/CanvasBackground.js
+++ components/CanvasBackground.js
@@ -10,8 +10,6 @@ const CanvasBackground = () => {
     const { elements, currentElement } = useContext(TableContext);
     const [isInitialized, setIsInitialized] = useState(false);
 
-
-
     // useEffect that runs only once to get the backghround squares colors on page load
     // Effect to update squares state when currentElement and elements are available
     // #####################################################        
@@ -20,16 +18,21 @@ const CanvasBackground = () => {
 
             console.log("USEEFFECT IF WORKING")
 
+            // Variables to setup the canvas
             const canvas = canvasRef.current;
             const ctx = canvas.getContext('2d');
+
+            // Variables to get the squares colors
             const { topLeftColor, topRightColor, bottomLeftColor, bottomRightColor } = getQuadrantColors(currentElement, elements);
 
+            // Canvas sizing
             canvas.width = window.innerWidth;
             canvas.height = window.innerHeight;
             ctx.clearRect(0, 0, canvas.width, canvas.height);
 
             console.log("topLeftColor: ", topLeftColor)
 
+            // Assign colors and positions to the initial squares
             const InitialSquares = [
                 { x: 0, y: 0, color: topLeftColor },
                 { x: window.innerWidth / 2, y: 0, color: topRightColor },
@@ -37,8 +40,7 @@ const CanvasBackground = () => {
                 { x: window.innerWidth / 2, y: window.innerHeight / 2, color: bottomRightColor }
             ];
 
-
-            // console.log("ALL SQUARES: ", allSquares)
+            // Iterate over each square to draw them in the canvas
             InitialSquares.forEach(square => {
                 // console.log('Square:', square);
                 if (square) {  // Check if square is not undefined
@@ -46,11 +48,9 @@ const CanvasBackground = () => {
                     ctx.fillRect(square.x, square.y, canvas.width / 2, canvas.height / 2);
                 }
             });
-            // After running your logic, set isInitialized to true
+            // Set isInitialized to true so this useEffect only runs once
             setIsInitialized(true);
         }
-
-
     }, [elements, currentElement, isInitialized]);
 
 
