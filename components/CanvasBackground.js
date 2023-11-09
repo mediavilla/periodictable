6904619495746d218bd9fc420f2063c6de9e59b1
Juan <jrmediavilla@gmail.com>
2023-11-09 21:17:37 +0000

    Canvas and initial squares separated

--- components/CanvasBackground.js
+++ components/CanvasBackground.js
@@ -1,8 +1,11 @@
 // CanvasBackground.js
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useContext } from 'react';
+import { TableContext } from '../utils/TableProvider';
+import { getQuadrantColors, getOffCanvasSquaresColors } from '../utils/getQuadrantColors';
 
 const CanvasBackground = () => {
     const canvasRef = useRef(null);
+    const { elements, currentElement, prevCol18Xpos, prevCol18Ypos } = useContext(TableContext);
 
     const resizeCanvas = () => {
         const canvas = canvasRef.current;
@@ -19,6 +22,28 @@ const CanvasBackground = () => {
         // Cleanup listener on unmount
         return () => window.removeEventListener('resize', resizeCanvas);
     }, []);
+
+
+
+    // #########################################################################################
+    // #########################################################################################
+    // Function to get the inital squares
+    const initialSQuares = () => {
+        const canvas = canvasRef.current; // Define canvas from the ref
+        const { topLeftColor, topRightColor, bottomLeftColor, bottomRightColor } = getQuadrantColors(currentElement, elements);
+        if (canvas) {
+            const squares = [
+                { x: 0, y: 0, color: topLeftColor },
+                { x: canvas.width / 2, y: 0, color: topRightColor },
+                { x: 0, y: canvas.height / 2, color: bottomLeftColor },
+                { x: canvas.width / 2, y: canvas.height / 2, color: bottomRightColor },
+            ];
+            return squares; // Return the array of squares
+        }
+        return []; // Return the array of squares
+    };
+
+    console.log("squares: ", initialSQuares())
 
     // Return the canvas element from the component
     return (
