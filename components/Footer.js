3bd7efafe31d62aba534c3d3ac71d5966660607e
Juan <jrmediavilla@gmail.com>
2023-12-02 21:55:06 +0000

    footer grid

--- components/Footer.js
+++ components/Footer.js
@@ -5,38 +5,20 @@ import footerStyles from '../styles/footer.module.css';
 export default function Footer() {
 
     return (
+
         <footer className={footerStyles.footerContainer}>
-            <div className={footerStyles.footer}>
-                <div className={footerStyles.footerLinks}>
-                    <ul className={footerStyles.footerList}>
-                        <li><Link href="#">Designs</Link></li>
-                        <li><Link href="#">Timeline</Link></li>
-                        <li><Link href="#">Elements</Link></li>
-                    </ul>
-                </div>
-                <div className={footerStyles.footerLinks}>
-                    <ul className={footerStyles.footerList}>
-                        <li><Link href="#">About</Link></li>
-                        <li><Link href="#">Nerd stuff</Link></li>
-                        <li><Link href="#">Download app</Link></li>
-                        <li><Link href="#">Shop</Link></li>
-                        <li><Link href="#">Lab</Link></li>
-                        <li><Link href="#">External resources</Link></li>
-                    </ul>
-                </div>
-                <div className={footerStyles.footerLinks}>
-                    <ul className={footerStyles.footerList}>
-                        <li><Link href="#">Cookie Policy</Link></li>
-                        <li><Link href="#">Terms and conditions</Link></li>
-                        <li><Link href="#">License</Link></li>
-                    </ul>
-                </div>
-                <div className={footerStyles.footerLinks}>
-                    <ul className={footerStyles.footerList}>
-                        <li><Link href="#">Social icons</Link></li>
-                    </ul>
-                </div>
-            </div>
+            <div className={`${footerStyles.title} ${footerStyles.title2}`}> Section 2 title</div >
+            <div className={footerStyles.icon}>Terms and Conditions</div>
+            <div className={`${footerStyles.section} ${footerStyles.section1Item}`}> Item 1</div >
+            <div className={`${footerStyles.section} ${footerStyles.section1Item}`}>Item 2</div>
+            <div className={`${footerStyles.section} ${footerStyles.section1Item}`}>Item 3</div>
+            <div className={`${footerStyles.section} ${footerStyles.section2Item}`}>Item 4</div>
+            <div className={`${footerStyles.section} ${footerStyles.section2Item}`}>Item 5</div>
+            <div className={`${footerStyles.section} ${footerStyles.section2Item}`}>Item 6</div>
+            <div className={footerStyles.bottomLink}>License</div>
+            <div className={footerStyles.bottomLink}>Cookie Policy</div>
+            <div className={footerStyles.bottomLink}>Terms and Conditions</div>
+            <div className={footerStyles.icon}>Terms and Conditions</div>
         </footer >
     );
 }
