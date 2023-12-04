a30869a5d834a54b0de6fcb81d93a732a9d797b7
Juan <jrmediavilla@gmail.com>
2023-12-04 22:59:40 +0000

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -8,13 +8,13 @@ export default function Footer() {
 
         <footer className={footerStyles.footer}>
             <div className={footerStyles.footerContainer}>
-                <div className={footerStyles.footerSectionTitle}> Section 1 title</div >
+                <div className={footerStyles.footerSectionTitle}> Design</div >
                 <div className={footerStyles.footerSectionTitle}> Section 2 title</div>
                 <div><Link href="#" className={footerStyles.bottomLink}>Social Media Icons</Link></div >
-                <div><Link href="#" className={footerStyles.bottomLink}>Item 2</Link></div>
+                <div><Link href="#" className={footerStyles.bottomLink}>Timeline</Link></div>
                 <div><Link href="#" className={footerStyles.bottomLink}>Item 3</Link></div>
                 <div>&nbsp;</div>
-                <div><Link href="#" className={footerStyles.bottomLink}>Item 5</Link></div>
+                <div><Link href="#" className={footerStyles.bottomLink}>Elements</Link></div>
                 <div><Link href="#" className={footerStyles.bottomLink}>Item 6</Link></div>
                 <div>&nbsp;</div>
                 <div><Link href="#" className={footerStyles.bottomLink}>License</Link></div>
