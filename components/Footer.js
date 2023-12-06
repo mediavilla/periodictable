f5f21c0bae89d807e7897657ee0a73943aac4be1
Juan <jrmediavilla@gmail.com>
2023-12-06 20:34:19 +0000

    More footer pages

--- components/Footer.js
+++ components/Footer.js
@@ -11,13 +11,16 @@ export default function Footer() {
             <div className={footerStyles.footerContainer}>
                 <div><Link href="#" className={footerStyles.bottomLink}>Design</Link></div >
                 <div><Link href="#" className={footerStyles.bottomLink}>Info / About</Link></div>
-                <div><Link href="#" className={footerStyles.bottomLink}>Social Media Icons</Link></div >
+                <div><Link href="#" className={footerStyles.bottomLink}>Apple Store</Link></div >
                 <div><Link href="#" className={footerStyles.bottomLink}>Timeline</Link></div>
                 <div><Link href="#" className={footerStyles.bottomLink}>Sponsor</Link></div>
-                <div><Link href="#" className={footerStyles.bottomLink}>Apple Store</Link></div>
+                <div><Link href="#" className={footerStyles.bottomLink}>Play Store</Link></div>
                 <div><Link href="#" className={footerStyles.bottomLink}>Elements</Link></div>
                 <div><Link href="#" className={footerStyles.bottomLink}>Donate</Link></div>
-                <div><Link href="#" className={footerStyles.bottomLink}>Play Store</Link></div>
+                <div><Link href="#" className={footerStyles.bottomLink}>Github</Link></div>
+                <div><Link href="#" className={footerStyles.bottomLink}>Feedback</Link></div>
+                <div><Link href="#" className={footerStyles.bottomLink}>Contact</Link></div>
+                <div><Link href="#" className={footerStyles.bottomLink}>Twitter [ X ]</Link></div>
                 <div><Link href="#" className={footerStyles.bottomLink}>License</Link></div>
                 <div><Link href="#" className={footerStyles.bottomLink}>Cookie Policy</Link></div>
                 <div><Link href="#" className={footerStyles.bottomLink}>Terms and Conditions</Link></div>
