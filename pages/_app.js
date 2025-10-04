c75d68b668896c78b724ec10085630fe7aea9029
Juan <jrmediavilla@gmail.com>
2025-10-04 22:57:51 +0100

    fixing image paths

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,20 @@
-import { Analytics } from '@vercel/analytics/react';
+import { useRouter } from 'next/router';
 import { TableProvider } from '../utils/TableProvider';
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-
+  const { basePath } = useRouter();
   return (
-    <TableProvider>
-      <Component {...pageProps} />
-      <Analytics />
-    </TableProvider>
+    <div
+      style={{
+        '--icon-18': `url("${basePath}/images/icon-18.svg")`,
+        '--icon-32': `url("${basePath}/images/icon-32.svg")`,
+        '--icon-rt': `url("${basePath}/images/icon-RT.svg")`,
+      }}
+    >
+      <TableProvider>
+        <Component {...pageProps} />
+      </TableProvider>
+    </div>
   );
 }
