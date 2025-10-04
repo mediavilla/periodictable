8fbd37c8252f2c2f11a2c911a612a7352a507119
Juan <jrmediavilla@gmail.com>
2025-10-04 19:03:43 +0100

    Moving to Github pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const isProd = process.env.NODE_ENV === 'production';
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+module.exports = {
+  output: 'export',
+  trailingSlash: true,
+  images: { unoptimized: true },
+  basePath: isProd ? '/periodictable' : '',
+  assetPrefix: isProd ? '/periodictable/' : '',
+};
